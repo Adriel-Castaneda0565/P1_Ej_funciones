a13c70ef98bef092540132cee84f4769c8524206
Adriel-Castaneda0565 <164534487+Adriel-Castaneda0565@users.noreply.github.com>
2025-02-07 16:47:39 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+// FUncion que no devulve valor
+// Funcion saludar
+void saludar(){
+  print("Hola mundo");
+}
+
+void escribeNombre(){
+  print("Adriel Castañeda ");
+}
+
+void main(){
+ //  Adriel Castañeda : 2230801280565
+ // Llamar a la funcion saludar
+ print('Adriel Castañeda : 2230801280565');
+ print("Llamando a la funcion saludar");
+ saludar();
+print("Llamando a la funcion escribeNombre");
+ escribeNombre();
+
+}
