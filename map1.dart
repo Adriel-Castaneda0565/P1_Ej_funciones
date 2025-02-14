c9fd4a797ec582ce5bdcdbc5010a36c1711e2c9d
Adriel-Castaneda0565 <164534487+Adriel-Castaneda0565@users.noreply.github.com>
2025-02-14 16:21:00 +0000

    ejemplo map listado No y el nombre del mes

---
+++ map1.dart
@@ -0,0 +1,31 @@
+void main() {
+  // Crear un Map con el número del mes (int) como clave y el nombre del mes (String) como valor
+  Map<int, String> mesesDelAnio = {
+    1: 'Enero',
+    2: 'Febrero',
+    3: 'Marzo',
+    4: 'Abril',
+    5: 'Mayo',
+    6: 'Junio',
+    7: 'Julio',
+    8: 'Agosto',
+    9: 'Septiembre',
+    10: 'Octubre',
+    11: 'Noviembre',
+    12: 'Diciembre',
+  };
+
+  // Imprimir el Map completo
+  print('Meses del año:');
+
+  // Acceder a un valor específico usando una clave
+  int numeroMes = 5;
+  String nombreMes = mesesDelAnio[numeroMes]!;
+  print('Mes $numeroMes: ${mesesDelAnio[nombreMes]}');
+  
+
+  // usando forEach
+  mesesDelAnio.forEach((numeroMes, nombreMes) {
+    print('Mes $numeroMes: $nombreMes');
+  });
+}
