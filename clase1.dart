7341ee8ba3d503c93fbcb3b4fbb515270e7ed1df
Adriel-Castaneda0565 <164534487+Adriel-Castaneda0565@users.noreply.github.com>
2025-02-17 18:15:08 +0000

    ejemplo clase

---
+++ clase1.dart
@@ -0,0 +1,35 @@
+class Persona {
+  // Atributos
+  String nombre;
+  int edad;
+
+  // Constructor
+  Persona(this.nombre, this.edad);
+
+  // Funciones (métodos)
+  void saludar() {
+    print('Hola, mi nombre es $nombre y tengo $edad años.');
+  }
+
+  void cumplirAnos() {
+    edad++;
+    print('¡Feliz cumpleaños! Ahora tengo $edad años.');
+  }
+}
+
+
+
+void main() {
+    print('Adriel castaneda: 2230805180565');
+  // Crear una instancia de la clase Persona
+  Persona persona = Persona('Adriel', 17);
+
+  // Acceder a los atributos
+  print('Nombre: ${persona.nombre}'); // Salida: Nombre: Juan
+  print('Edad: ${persona.edad}');     // Salida: Edad: 30
+
+  // Llamar a las funciones
+  persona.saludar(); // Salida: Hola, mi nombre es Juan y tengo 30 años.
+  persona.cumplirAnos(); // Salida: ¡Feliz cumpleaños! Ahora tengo 31 años.
+
+}
